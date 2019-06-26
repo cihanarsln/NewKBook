626d0094556a30bdf7ec023d25ae39fd23b889bc
cihanarslan <32307607+cihanarslan@users.noreply.github.com>
2019-06-26 18:11:07 +0300

    Initial Commit: Basic changes.

--- src/main/java/com/kafein/kbook/config/SpringSecurityConfig.java
+++ src/main/java/com/kafein/kbook/config/SpringSecurityConfig.java
@@ -1,7 +1,6 @@
 package com.kafein.kbook.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,15 +31,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
-
-                /*.httpBasic()
-                .and()
-                .authorizeRequests()
-                .antMatchers(HttpMethod.DELETE, "/kbook/admins/**").hasRole("ADMIN")
-                .antMatchers("/**").permitAll().anyRequest().authenticated();
-                .and()
-                .csrf().disable()
-                .formLogin().disable(); */
     }
 
 
