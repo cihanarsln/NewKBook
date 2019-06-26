6c45a64e5b14562ca37f2285df14d9d160e1dc1a
cihanarslan <32307607+cihanarslan@users.noreply.github.com>
2019-06-26 18:13:16 +0300

    Initial Commit: Basic changes.

---
+++ src/main/java/com/kafein/kbook/config/ServerSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.kafein.kbook.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@Order
+@Import(Encoders.class)
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+}
